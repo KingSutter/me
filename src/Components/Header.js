aefe9cd00620cf43329f8782fd62717bc1bed9c3
Will Sutter <kingsutter12@gmail.com>
2023-10-17 11:13:18 -0600

    Updated resume and personal info

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">Hi, I'm {name}.</h1>
-            <h3>I'm an {city} based {occupation}. {description}.</h3>
+            <h3>I'm a {city} based {occupation}. {description}.</h3>
             <hr />
             <ul className="social">
                {networks}
